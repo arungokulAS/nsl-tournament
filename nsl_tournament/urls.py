2533017c11c05e58bc5113454d08a7a908764c9e
Arun Gokul <arungokul@Aruns-MacBook-Pro.local>
2026-01-25 17:42:13 +0000

    Add favicon and static files config

--- nsl_tournament/urls.py
+++ nsl_tournament/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
