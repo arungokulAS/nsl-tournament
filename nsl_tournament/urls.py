f6b8ec304708aeecab847c41aca71032b479a605
Arun Gokul <arungokul@Aruns-MacBook-Pro.local>
2026-01-25 17:45:36 +0000

    Add home view and root URL pattern

--- nsl_tournament/urls.py
+++ nsl_tournament/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
 ]
